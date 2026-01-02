85e3b5b8c8f8e882f07095b554f99f341d70aaf4
Priom Sarkar <priomsarker02@gmail.com>
2026-01-03 00:04:59 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.restaurantmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.restaurantmanagement to javafx.fxml;
+    exports com.example.restaurantmanagement;
+}
